c1fbe3c20f193f9a71a83beb17036bebf46c7d74
Marqell Broxton <56138615+marqellbroxton@users.noreply.github.com>
2021-08-02 08:13:18 -0400

    Add files via upload

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Hero = () => {
+    return (
+        <section className="hero">
+            <h2>Web Service Books</h2>
+            <h3>A mind without knowledge is <br />like a car without gas</h3>
+            <Link className="btn" to="/books">View All Books</Link>
+        </section>
+    )
+}
+
+export default Hero
